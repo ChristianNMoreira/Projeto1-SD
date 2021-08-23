89ae810d477fcb33f77bf2e6601429fc31793f35
Christian <cmoreira589@gmail.com>
2021-08-23 14:03:18 -0300

    :two: Add 2 complement for n bits

--- ULA.vhd
+++ ULA.vhd
@@ -29,20 +29,16 @@ component overflow_detector port (
 		);
 end component;
 
-component complement2_4bit port ( 
-		X: in std_logic_vector(3 downto 0);
-		Z: out std_logic_vector(3 downto 0)
-		);
-end component;
-
 signal sum: std_logic_vector(n-1 downto 0); -- sum 4 bits
 signal aux_sum: std_logic_vector(n downto 0);
 signal sum1: std_logic_vector(n-1 downto 0); -- 1 implement
 signal aux_sum1: std_logic_vector(n downto 0);
-signal sub: std_logic_vector(n-1 downto 0);
+signal sub: std_logic_vector(n-1 downto 0); -- subtraction
 signal aux_sub: std_logic_vector(n downto 0);
-signal comp2: std_logic_vector(3 downto 0);
+signal comp2: std_logic_vector(n-1 downto 0); -- 2 complement
+signal aux_comp2: std_logic_vector(n-1 downto 0);
 signal cts: std_logic_vector(8 downto 0); -- carry outs
+signal not_a1: std_logic_vector(n-1 downto 0);
 signal not_a2: std_logic_vector(n-1 downto 0);
 constant zeros : std_logic_vector(n-1 downto 0) := ( others => '0');
 begin
@@ -77,8 +73,17 @@ begin
 		OVERF: overflow_detector port map (A => A1(n-1), B => A2(n-1), Z => sub(n-1), O => OV);
 		
 		-- Complemento de 2 (Troca de sinal)
-		COMP_2: complement2_4bit port map(X => A1, Z => comp2);
+		L2: for i in 0 to (n-1) generate
+			not_a1(i) <= not A1(i);
+		end generate L2;
+		aux_comp2(0) <= '0';
+		COMP_2_0: full_adder port map (A => not_a1(0), B => '1', Cin => aux_comp2(0), S => comp2(0), Cout => aux_comp2(1));
+		COMP_2: for i in 1 to (n-2) generate
+			 COMP_2_i: full_adder port map (A => not_a1(i), B => '0', Cin => aux_comp2(i), S => comp2(i), Cout => aux_comp2(i+1));
+		end generate;
+		COMP_2_final: full_adder port map (A => not_a1(n-1), B => '0', Cin => aux_comp2(n-1), S => comp2(n-1), Cout => open);
 		
+		-- Saídas
 		E1 <= sum when (K="000") else
 			   sum1 when (K="001") else
 				sub when (K="010") else
