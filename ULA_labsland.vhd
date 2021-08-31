849e6e5769138b796d9094e10f557adcb66bcecd
Xavier <eduardoxavierrf@gmail.com>
2021-08-30 21:51:41 -0300

    ULA adaptada para labsland

--- ULA_labsland.vhd
+++ ULA_labsland.vhd
@@ -14,15 +14,9 @@ entity ULA_labsland is
 	-- OV: 
 
 	port (
-		-- Cin: in std_logic;
-		-- A, B: in std_logic_vector(n-1 downto 0);
-		-- K: in std_logic_vector (2 downto 0);
-		-- Cout: out std_logic;
-		-- OV: out std_logic; --Overflow
-		-- Y: out std_logic_vector(n-1 downto 0)
 		SW: in std_logic_vector(17 downto 0);
 		--KEY: in std_logic_vector(2 downto 0);
-		LEDG: out std_logic_vector(0 downto 0);
+		LEDG: out std_logic_vector(0 downto 3);
 		HEX3: out std_logic_vector(6 downto 0);
 		HEX2: out std_logic_vector(6 downto 0);
 		HEX0: out std_logic_vector(6 downto 0)
@@ -61,7 +55,15 @@ signal Cins: std_logic_vector(n downto 0);
 signal Ss: std_logic_vector(n-1 downto 0);
 signal K: std_logic_vector(2 downto 0);
 signal Y: std_logic_vector(n-1 downto 0);
-constant zeros : std_logic_vector(n-1 downto 0) := ( others => '0');
+signal O: std_logic;
+
+signal bit_or_bit: std_logic_vector(n-1 downto 0);
+signal bit_and_bit: std_logic_vector(n-1 downto 0);
+signal bit_xor_bit: std_logic_vector(n-1 downto 0);
+signal bit_nand_bit: std_logic_vector(n-1 downto 0);
+
+signal resultado: std_logic_vector(n-1 downto 0);
+signal detect_zero: std_logic_vector(n-1 downto 0);
 
 begin
 
@@ -85,18 +87,37 @@ begin
 	Cins(0) <= D(6) or D(5) or D(4);
 
 	FA3: for i in 0 to (n-1) generate
+		bit_or_bit(i) <= SW(3+i) or SW(3+n+i);
+		bit_and_bit(i) <= SW(3+i) and SW(3+n+i);
+		bit_xor_bit(i) <= SW(3+i) xor SW(3+n+i);
+		bit_nand_bit(i) <= SW(3+i) nand SW(3+n+i);
+		
 		As(i) <= ( SW(3+i) and (not D(4)) ) or ( (not SW(3+i)) and (not D(7)) and (not D(6)) and (not D(5)) );
 		Bs(i) <= ( SW(3+n+i) and (not D(6)) and (not D(4)) and (not D(5)) ) or
 					( (not SW(3+n+i)) and (not D(7)) and (not D(6)) and (not D(4)));
 		FA3_i: full_adder port map (A => As(i), B => Bs(i), Cin => Cins(i), S => Ss(i), Cout => Cins(i+1));
 	end generate;
+	
+	OVERF: overflow_detector port map (A => SW(3+n-1), B => SW(3+n+n-1), Z => Ss(n-1), O => O);
+	
+	result: for i in 0 to (n-1) generate
+		resultado(i) <= (Ss(i) and (D(7) or D(6) or D(5) or D(4))) or (bit_or_bit(i) and D(3)) or (bit_xor_bit(i) and D(2)) or (bit_and_bit(i) and D(1)) or (bit_nand_bit(i) and D(0));
+	end generate;
+	
+	detect_zero(0) <= resultado(0);
+	zero: for i in 1 to (n-1) generate
+		detect_zero(i) <= detect_zero(i-1) or resultado(i);
+	end generate;
 
-	Y <= Ss;
+	Y <= resultado;
 	H3: decoder_hex port map (X =>Y, H => HEX0);
 	LEDG(0) <= Cins(n);
-
+	-- Detecta numeros negativos nas operaçoes de subtraçao e troca de sinal
+	LEDG(1) <= Ss(n-1) and (D(4) or D(5));
+	-- Detecta overflow na operaçao de subtracao
+	LEDG(2) <= O and D(4);
+	-- Detecta zeros
+	LEDG(3) <= not detect_zero(n-1);
 	
-	--OVERF: overflow_detector port map (A => A1(n-1), B => A2(n-1), Z => sub(n-1), O => OV);
-
 	
 end teste;
